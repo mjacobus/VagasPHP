17019526e51b76dff92142b837b46a90f1c5c49b
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-11-18 17:09:05 +0100

    Changed capistrano configs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,11 +1,16 @@
 set :stage, :production
 set :application, 'vagasphp.com.br'
 set :deploy_to, "/home/vagasphp/sites/#{fetch(:application)}"
+set :branch, 'master'
 
-set :user, ask('Username', 'vagasphp')
+# set :user, ask('Username', 'vagasphp')
+set :user, 'vagasphp'
 set :password, ask('Password', nil)
 
 server 'vagasphp.com.br',
   user: fetch(:user),
-  password: fetch(:password),
-  roles: %w{web app db}
+  roles: %w{web app db},
+  ssh_options: {
+    # keys: [fetch(:ssh_key)],
+    auth_methods: %w(publickey)
+  }
